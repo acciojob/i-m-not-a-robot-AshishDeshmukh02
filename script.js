bfcf5b662db00316bef9b2e6849e103961650912
Ashish Deshmukh <115146337+AshishDeshmukh02@users.noreply.github.com>
2023-02-10 17:05:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your JS code here. If required.
+// Global Variables
+let selectedImages = [];
+let images = [];
+let identicalImageClass;
+
+// Function to randomly arrange the images
+function shuffleImages() {
+  // Code to randomly arrange the images
+}
+
+// Function to select an image
+function selectImage(id) {
+  // Code to select an image
+  // Add the selected image to the selectedImages array
+  // If two images have been selected, show the verify button
+  // If the same image is clicked twice, remove it from the selectedImages array
+}
+
+// Function to reset the state
+function resetState() {
+  // Code to reset the state to initial state
+  // Hide the reset button and the verify button
+  // Empty the selectedImages array
+}
+
+// Function to verify the selected images
+function verifyImages() {
+  // Code to verify the selected images
+  // If the selected images are identical, show the message "You are a human. Congratulations!"
+  // If the selected images are different, show the message "We can't verify you as a human. You selected the non-identical tiles."
+}
+
+// On Load
+window.onload = function() {
+  shuffleImages();
+  // Code to select a random image to repeat
+  identicalImageClass = "img1";
+}
+
+// Event Listeners
+document.getElementById("reset").addEventListener("click", resetState);
+document.getElementById("verify").addEventListener("click", verifyImages);
